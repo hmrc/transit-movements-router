44074dce23257918c3353a82d5e3d1ab8960eb69
Derek Palmer <35488891+derekspalmer@users.noreply.github.com>
2022-06-13 08:32:01 +0100

    CTCP-514-service: FanOut on build message

--- test/uk/gov/hmrc/transitmovements/router/services/XmlParserSpec.scala
+++ test/uk/gov/hmrc/transitmovements/router/services/XmlParserSpec.scala
@@ -1,24 +1,35 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.transitmovements.router.services
 
-import akka.stream.alpakka.xml.scaladsl.XmlWriting
-import akka.stream.alpakka.xml.scaladsl.XmlParsing
+import akka.stream.alpakka.xml.scaladsl._
 import akka.stream.scaladsl.Sink
 import akka.util.ByteString
 import org.scalatest.concurrent.ScalaFutures.whenReady
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
-import uk.gov.hmrc.transitmovements.base.StreamTestHelpers.createParsingEventStream
-import uk.gov.hmrc.transitmovements.base.StreamTestHelpers.createStream
+import uk.gov.hmrc.transitmovements.base.StreamTestHelpers._
 import uk.gov.hmrc.transitmovements.base.TestActorSystem
-import uk.gov.hmrc.transitmovementsrouter.models.EoriNumber
-import uk.gov.hmrc.transitmovementsrouter.models.MessageSenderId
-import uk.gov.hmrc.transitmovementsrouter.models.MovementMessageId
+import uk.gov.hmrc.transitmovementsrouter.models._
 import uk.gov.hmrc.transitmovementsrouter.services.XmlParser
 
-import scala.xml.NodeSeq
-import scala.xml.XML
 import scala.xml.Utility.trim
+import scala.xml._
 
 class XmlParserSpec extends AnyFreeSpec with TestActorSystem with Matchers {
 
@@ -57,7 +68,7 @@ class XmlParserSpec extends AnyFreeSpec with TestActorSystem with Matchers {
       val parsedResult = stream.via(XmlParser.officeOfDestinationExtractor).runWith(Sink.head)
 
       whenReady(parsedResult) {
-        _.right.get mustBe EoriNumber("GB6789")
+        _.right.get mustBe OfficeOfDestination("GB6789")
       }
     }
 
@@ -80,7 +91,7 @@ class XmlParserSpec extends AnyFreeSpec with TestActorSystem with Matchers {
     }
   }
 
-  "create MessageSenderElement" in new Setup {
+  "MessageSenderElement parser should add the messageSender node with the movement Id" in new Setup {
 
     val stream = createStream(cc015cNoMessageSenderNode)
 
@@ -94,7 +105,6 @@ class XmlParserSpec extends AnyFreeSpec with TestActorSystem with Matchers {
 
     whenReady(parsedResult) {
       result =>
-        println(trim(XML.loadString(result)))
         trim(XML.loadString(result)) shouldBe trim(cc015cWithExpectedMessageSenderNode.head)
     }
   }
