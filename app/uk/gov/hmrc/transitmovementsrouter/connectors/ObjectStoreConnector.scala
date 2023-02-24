9b136c9a52fb6157f779cff9205403b24e11ae90
olakunlekuye2 <100710237+olakunlekuye2@users.noreply.github.com>
2023-02-24 12:37:15 +0000

    Modified the ether return

--- app/uk/gov/hmrc/transitmovementsrouter/connectors/ObjectStoreConnector.scala
+++ app/uk/gov/hmrc/transitmovementsrouter/connectors/ObjectStoreConnector.scala
@@ -1,107 +1,64 @@
-/*
- * Copyright 2023 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package uk.gov.hmrc.transitmovementsrouter.connectors
 
-import cats.data.EitherT
-import cats.implicits._
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import com.google.inject.ImplementedBy
+import play.api.Logging
 import play.api.mvc.Result
 import play.api.mvc.Results
 import uk.gov.hmrc.http.HeaderCarrier
-import uk.gov.hmrc.objectstore.client.Md5Hash
-import uk.gov.hmrc.objectstore.client.ObjectMetadata
+import uk.gov.hmrc.http.UpstreamErrorResponse
 import uk.gov.hmrc.objectstore.client.ObjectSummaryWithMd5
-import uk.gov.hmrc.objectstore.client.RetentionPeriod
+import uk.gov.hmrc.objectstore.client.Path
+import uk.gov.hmrc.objectstore.client.play.Implicits._
+import uk.gov.hmrc.objectstore.client.play.PlayObjectStoreClientEither
 import uk.gov.hmrc.transitmovementsrouter.models.MessageId
 import uk.gov.hmrc.transitmovementsrouter.models.MovementId
 
-import java.io.File
-import java.time.Instant
 import javax.inject.Inject
-import javax.inject.Singleton
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
-trait Path[A] {
-  def path: String
+
+
+@ImplementedBy(classOf[ObjectStoreConnectorImpl])
+trait ObjectStoreConnector {
+
+  def putObjectStoreFile(movementId: MovementId, messageId: MessageId, path: java.nio.file.Path)(implicit
+                                                                                                 ec: ExecutionContext,
+                                                                                                 hc: HeaderCarrier
+  ): Future[Either[Result, ObjectSummaryWithMd5]]
+
 }
 
-object Path {
-  case class Directory(path: String) extends Path[Directory]
 
-  case class File(parent: Path[Directory], filename: String) extends Path[File] {
-    def path: String = parent.path + "/" + filename
-  }
-}
+class ObjectStoreConnectorImpl @Inject() (client: PlayObjectStoreClientEither)(implicit ec: ExecutionContext) extends ObjectStoreConnector with Logging {
 
-trait ObjectStoreContentWrite[F[_], CONTENT, BODY] {
-  def writeContent(content: CONTENT, contentType: Option[String], contentMd5: Option[Md5Hash]): F[BODY]
-}
-
-trait PlayObjectStoreClientEither[F[_]] {
-
-  def putObject[BODY](
-    file: Path[Path.File],
-    ioFile: File,
-    retentionPeriod: RetentionPeriod,
-    maybeContentEncoding: Option[String],
-    maybeContentMd5: Option[Md5Hash]
-  )(
-    contentWriter: ObjectStoreContentWrite[F, File, BODY],
-    hc: HeaderCarrier
-  )(implicit ec: ExecutionContext): EitherT[F, Throwable, ObjectSummaryWithMd5]
-}
-
-@Singleton
-class ObjectStoreConnector @Inject() (
-  client: PlayObjectStoreClientEither[Future]
-)(implicit ec: ExecutionContext) {
-
-  def upload(movementId: MovementId, messageId: MessageId, path: java.nio.file.Path)(implicit
-    hc: HeaderCarrier
-  ): EitherT[Future, Result, ObjectSummaryWithMd5] = {
-    val objectMetadata = new ObjectMetadata("", 0, new Md5Hash("md5Hash"), Instant.now(), Map.empty)
-
-    val directory = Path.Directory(s"movements/${movementId.value}/messages")
-
-    val file = Path.File(directory, s"${messageId.value}.xml")
-    val metadata = ObjectMetadata(
-      contentType = "application/xml",
-      contentLength = path.toFile.length(),
-      contentMd5 = new Md5Hash("md5-hash-string"),
-      lastModified = Instant.now(),
-      userMetadata = Map.empty[String, String]
-    )
-
+  override def putObjectStoreFile(movementId: MovementId, messageId: MessageId, path: java.nio.file.Path)(implicit
+                                                                                                 ec: ExecutionContext,
+                                                                                                 hc: HeaderCarrier
+  ): Future[Either[Result, ObjectSummaryWithMd5]] = {
     client
-      .putObject[Array[Byte]](
-        file = file,
-        ioFile = path.toFile,
-        retentionPeriod = RetentionPeriod.OneMonth,
-        maybeContentEncoding = Some(metadata.contentType),
-        maybeContentMd5 = Some(metadata.contentMd5)
-      )(
-        contentWriter = new ObjectStoreContentWrite[Future, File, Array[Byte]] {
-          override def writeContent(content: File, contentType: Option[String], contentMd5: Option[Md5Hash]): Future[Array[Byte]] =
-            Future.successful(Array.emptyByteArray)
-        },
-        hc = hc
+      .putObject(
+        path = Path.Directory(s"movements/${movementId.value}/messages").file(s"${messageId.value}.xml"),
+        content = path.toFile,
+        contentType = Some("application/xml")
       )
-      .leftMap(
-        _ => Results.InternalServerError("An error has occurred.")
-      )
+      .map {
+        case Right(summary) => Right(summary)
+        case Left(exception) =>
+          logger.error(s"An error has occurred: ${exception.getMessage}", exception)
+          Left(Results.InternalServerError("An error has occurred."))
+      }
+      .recover {
+        case e: UpstreamErrorResponse =>
+          logger.error(s"Upstream error: ${e.getMessage}")
+          Left(Results.InternalServerError("Upstream error encountered"))
+        case e: Exception =>
+          logger.error(s"An error has occurred: ${e.getMessage}", e)
+          Left(Results.InternalServerError("An error has occurred."))
+      }
   }
+
 }
+
--- test/uk/gov/hmrc/transitmovementsrouter/connectors/ObjectStoreConnectorSpec.scala
+++ test/uk/gov/hmrc/transitmovementsrouter/connectors/ObjectStoreConnectorSpec.scala
@@ -3,39 +3,48 @@ package uk.gov.hmrc.transitmovementsrouter.connectors
 import cats.data.EitherT
 import cats.implicits._
 import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.{eq => eqTo}
+import org.mockito.Mockito.verify
 import org.mockito.Mockito.when
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.mockito.MockitoSugar
+import play.api.mvc.Result
 import play.api.mvc.Results
 import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.objectstore.client.Path.Directory
 import uk.gov.hmrc.objectstore.client._
-import uk.gov.hmrc.transitmovementsrouter.models.{MessageId, MovementId}
+import uk.gov.hmrc.transitmovementsrouter.models.MessageId
+import uk.gov.hmrc.transitmovementsrouter.models.MovementId
 
-import java.io.File
-import java.nio.file.{Files, Path}
+import java.io.{File => JFile}
+import java.nio.file.Files
+import java.nio.file.Path
 import java.time.Instant
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 
 class ObjectStoreConnectorSpec extends AnyFreeSpec with Matchers with ScalaFutures with MockitoSugar {
 
+  val testPath: Path  = Files.createTempFile("test", ".xml")
+  val testFile: JFile = testPath.toFile
+
   trait Test {
-    implicit val ec = scala.concurrent.ExecutionContext.global
+    val testFile: JFile = testPath.toFile
 
     val mockClient: PlayObjectStoreClientEither[Future] = mock[PlayObjectStoreClientEither[Future]]
-    val connector = new ObjectStoreConnector(mockClient)
-    val hc = HeaderCarrier()
-    val movementId = MovementId("123")
-    val messageId = MessageId("abc")
-    val testPath: Path = Files.createTempFile("test", "xml")
+    val connector                                       = new ObjectStoreConnector(mockClient)
+    val hc                                              = HeaderCarrier()
+    val movementId                                      = MovementId("123")
+    val messageId                                       = MessageId("abc")
+    val testPath: Path                                  = Files.createTempFile("test", ".xml")
 
-    def verifyInternalServerError(result: EitherT[Future, Results.Status, ObjectSummaryWithMd5]): Unit = {
-      result.leftMap(_ => Results.InternalServerError(""))
-      result.value.futureValue.left.map { res =>
-        res.header.status shouldBe Results.InternalServerError.header.status
-      }
-    }
+    def verifyInternalServerError(result: EitherT[Future, Result, ObjectSummaryWithMd5]): Unit =
+      result.leftMap(
+        _ => Results.InternalServerError("")
+      )
   }
 
   "ObjectStoreConnector" - {
@@ -46,21 +55,18 @@ class ObjectStoreConnectorSpec extends AnyFreeSpec with Matchers with ScalaFutur
 
         when(
           mockClient.putObject[Array[Byte]](
-            any(),
-            any(),
-            any(),
-            any(),
-            any()
-          )(any(), any())(any())
+            Path.File(Directory("movements/123/messages"), "abc.xml"),
+            testPath.toFile,
+            RetentionPeriod.OneMonth,
+            Some("application/xml"),
+            Some(new Md5Hash("md5-hash-string"))
+          )(any[ExecutionContext], any[HeaderCarrier])
         ).thenReturn(
           EitherT.rightT[Future, Throwable](
             ObjectSummaryWithMd5(
-              "",
-              "",
-              new Md5Hash("md5-hash-string"),
-              "application/xml",
+              Path.File(Directory("movements/123/messages"), "abc.xml"),
               testPath.toFile.length(),
-              RetentionPeriod.OneMonth,
+              new Md5Hash("md5-hash-string"),
               Instant.now()
             )
           )
@@ -72,12 +78,12 @@ class ObjectStoreConnectorSpec extends AnyFreeSpec with Matchers with ScalaFutur
         verify(
           mockClient
         ).putObject[Array[Byte]](
-          Path.File(Path.Directory(s"movements/${movementId.value}/messages"), s"${messageId.value}.xml"),
+          Path.File(Directory("movements/123/messages"), "abc.xml"),
           testPath.toFile,
           RetentionPeriod.OneMonth,
           Some("application/xml"),
           Some(new Md5Hash("md5-hash-string"))
-        )(any(), eqTo(hc))(any())
+        )(any[ExecutionContext], eqTo(hc))
 
       }
 
@@ -85,15 +91,15 @@ class ObjectStoreConnectorSpec extends AnyFreeSpec with Matchers with ScalaFutur
 
         when(
           mockClient.putObject[Array[Byte]](
-            any(),
-            any(),
-            any(),
-            any(),
-            any()
-          )(any(), any())(any())
+            any[Path.File],
+            any[JFile],
+            any[RetentionPeriod],
+            any[Option[String]],
+            any[Option[Md5Hash]]
+          )(any[ExecutionContext], any[HeaderCarrier])
         ).thenReturn(
           EitherT.leftT[Future, ObjectSummaryWithMd5](
-            new RuntimeException("Something went wrong")
+            Results.InternalServerError("")
           )
         )
 
@@ -101,7 +107,6 @@ class ObjectStoreConnectorSpec extends AnyFreeSpec with Matchers with ScalaFutur
         result.value.futureValue shouldBe a[Left[_, _]]
 
         verifyInternalServerError(result)
-
       }
     }
   }
