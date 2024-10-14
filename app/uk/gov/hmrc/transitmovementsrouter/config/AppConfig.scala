8240653c3a1adbacc4386de357feeb7d0b51febd
damien.butterworth <damienbutterworth@hotmail.co.uk>
2024-10-14 15:58:55 +0100

    format code

--- app/uk/gov/hmrc/transitmovementsrouter/config/AppConfig.scala
+++ app/uk/gov/hmrc/transitmovementsrouter/config/AppConfig.scala
@@ -29,10 +29,10 @@ class AppConfig @Inject() (config: Configuration, servicesConfig: CTCServicesCon
   lazy val appName: String = config.get[String]("appName")
 
   lazy val forceTransitionalInflight: Boolean = config.get[Boolean]("force-transitional-inflight")
-  lazy val eisXi: EISInstanceConfig     = config.get[EISInstanceConfig]("microservice.services.eis.xi")
-  lazy val eisGb: EISInstanceConfig     = config.get[EISInstanceConfig]("microservice.services.eis.gb")
-  lazy val eisGbV2_1: EISInstanceConfig = config.get[EISInstanceConfig]("microservice.services.eis.gb_v2_1")
-  lazy val eisXiV2_1: EISInstanceConfig = config.get[EISInstanceConfig]("microservice.services.eis.xi_v2_1")
+  lazy val eisXi: EISInstanceConfig           = config.get[EISInstanceConfig]("microservice.services.eis.xi")
+  lazy val eisGb: EISInstanceConfig           = config.get[EISInstanceConfig]("microservice.services.eis.gb")
+  lazy val eisGbV2_1: EISInstanceConfig       = config.get[EISInstanceConfig]("microservice.services.eis.gb_v2_1")
+  lazy val eisXiV2_1: EISInstanceConfig       = config.get[EISInstanceConfig]("microservice.services.eis.xi_v2_1")
 
   lazy val persistenceServiceBaseUrl: Url = Url.parse(servicesConfig.baseUrl("transit-movements"))
 
