117542a953cdd3c2441b329735a4b0ddf953734b
damien.butterworth <damienbutterworth@hotmail.co.uk>
2024-12-17 10:13:45 +0000

    update coverage and add models to exclusions

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -11,7 +11,8 @@ object CodeCoverageSettings {
     "prod.*",
     ".*Routes.*",
     "testOnly.*",
-    "testOnlyDoNotUseInAppConf.*"
+    "testOnlyDoNotUseInAppConf.*",
+    ".*models.*"
   )
 
   val settings: Seq[Setting[?]] = Seq(
